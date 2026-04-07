1ca3e559aa857142aff05aaec28d811f3445e78a
deftorch <deftorchestrator@gmail.com>
2026-04-07 13:45:51 +0700

    docs: Update README and add images screenshot for better documentation

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,15 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:pertemuan6/main.dart';
+import 'package:pertemuan6/main.dart' as app;
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('App smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MaterialApp());
+    await tester.pumpWidget(MaterialApp(home: app.BookListScreen()));
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Verify that our book list is rendered
+    expect(find.text('Book List'), findsOneWidget);
+    expect(find.text('Algorithms to Live By: The Computer Science of Human Decisions'), findsOneWidget);
   });
 }
